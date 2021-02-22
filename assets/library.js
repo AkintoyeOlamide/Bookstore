c2cb9eade1fb0a75bab3994bc4db9e77d3f54f22
AkintoyeOlamide <akintoyeolamide500@gmail.com>
2021-02-22 18:26:58 +0100

    created the cards and set the functions that allows the card to ahow when froms are created

--- assets/library.js
+++ assets/library.js
@@ -1,10 +1,45 @@
 let myLibrary = [];
+const domParser = new DOMParser()
+const bookRow = document.getElementById('bookRow')
 
-function Book(title, author, readStatus, pages) {
+
+function Book(title, author, read, pages) {
   this.title = title
   this.author = author
-  this.readStatus = readStatus
+  this.read = read
   this.pages = pages
+}
+
+myLibrary.push(
+  new Book('The Politics', 'Sigmund F.', true, 442),
+  new Book('The hobbit', "JR.Jenkins", true, 786),
+  new Book('Hard kill', "lotert runway", false, 235)
+)
+
+myLibrary.forEach((book)=> {
+  bookRow.appendChild(bookCard(book))
+})
+
+function bookCard(book) {
+  const card = `
+
+  <div class="col-6">
+  <div class="card h-100">
+    <div class="card-body">
+      <h4 class="mb-0">${book.title}</h4>
+      <h6>${book.author}</h6>
+      <p>Pages: ${book.pages}</p>
+      <p>Read: ${book.read}   
+      <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+    <label class="form-check-label" for="flexSwitchCheckChecked"></label>
+   </p>
+    </div>
+  </div>
+</div>
+
+
+  `
+  return domParser.parseFromString(card, 'text/html').body.children[0]
 }
 
 const bookForm = document.getElementById('bookForm')
@@ -17,24 +52,10 @@ function addBookToLibrary(event) {
   const newBook = new Book(
     title = formData.get('title'),
     author = formData.get('author'),
-    readStatus = formData.get('readStatus'),
+    read = Boolean(formData.get('read')),
     pages = formData.get('pages')
   )
-
-  const bookRow = document.getElementById('bookRow')
-  const bookCard = `
-    <div class="col-6">
-      <div class="card h-100">
-        <div class="card-body">
-          <h4 class="mb-0">${newBook.title}</h4>
-          <h6>${newBook.author}</h6>
-          <p>Pages: ${newBook.pages}</p>
-          <p>Read status: ${newBook.readStatus}</p>
-        </div>
-      </div>
-    </div>
-  `
-  const domParser = new DOMParser()
-  bookRow.appendChild(domParser.parseFromString(bookCard, 'text/html').body.children[0])
+   
+  bookRow.appendChild(bookCard(newBook))
   bookForm.reset()
 }
