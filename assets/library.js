efe96b12e4845b08f7a88d2f35beaadfa03a3164
CY Kalu <cy23@tutanota.com>
2021-02-22 15:44:37 +0100

    Append new book to the document

--- assets/library.js
+++ assets/library.js
@@ -11,13 +11,30 @@ const bookForm = document.getElementById('bookForm')
 bookForm.addEventListener("submit", addBookToLibrary)
 
 function addBookToLibrary(event) {
-    event.preventDefault();
-  const newBook = Book(  
-      title = document.getElementById("title").value,
-      author = document.getElementById("author").value,
-      readStatus = document.getElementById("readStatus").value,
-      pages = document.getElementById("pages").value
-   )
-   console.log(newBook)
-}
+  event.preventDefault();
 
+  const formData = new FormData(bookForm)
+  const newBook = new Book(
+    title = formData.get('title'),
+    author = formData.get('author'),
+    readStatus = formData.get('readStatus'),
+    pages = formData.get('pages')
+  )
+
+  const bookRow = document.getElementById('bookRow')
+  const bookCard = `
+    <div class="col-6">
+      <div class="card h-100">
+        <div class="card-body">
+          <h4 class="mb-0">${newBook.title}</h4>
+          <h6>${newBook.author}</h6>
+          <p>Pages: ${newBook.pages}</p>
+          <p>Read status: ${newBook.readStatus}</p>
+        </div>
+      </div>
+    </div>
+  `
+  const domParser = new DOMParser()
+  bookRow.appendChild(domParser.parseFromString(bookCard, 'text/html').body.children[0])
+  bookForm.reset()
+}
