40f5779a3d3fa428d61fc656bef20a15267ffd13
CY Kalu <cy23@tutanota.com>
2021-02-23 15:25:24 +0100

    Fix linter errors

--- assets/library.js
+++ assets/library.js
@@ -1,27 +1,62 @@
-let myLibrary = []
-const bookForm = document.getElementById('bookForm')
-const bookRow = document.getElementById('bookRow')
+const myLibrary = [];
+const bookForm = document.getElementById('bookForm');
+const bookRow = document.getElementById('bookRow');
 
 function Book(title, author, read, pages) {
-  this.title = title
-  this.author = author
-  this.read = read
-  this.pages = pages
+  this.title = title;
+  this.author = author;
+  this.read = read;
+  this.pages = pages;
 }
 
 myLibrary.push(
   new Book('The Politics', 'Sigmund F.', true, 442),
-  new Book('The hobbit', "JR.Jenkins", true, 786),
-  new Book('Hard kill', "lotert runway", false, 235)
-)
+  new Book('The hobbit', 'JR.Jenkins', true, 786),
+  new Book('Hard kill', 'lotert runway', false, 235)
+);
 
-myLibrary.forEach((book) => {
-  bookRow.appendChild(bookCard(book))
-})
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
+    const random = Math.random() * 16 | 0;
+    const v = char === 'x' ? random : (random & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function checker(book) {
+  return book.read ? 'checked' : '';
+}
+
+function addBookToLibrary(event) {
+  event.preventDefault();
+
+  const formData = new FormData(bookForm);
+  const newBook = new Book(
+    title = formData.get('title'),
+    author = formData.get('author'),
+    read = Boolean(formData.get('read')),
+    pages = formData.get('pages')
+  );
+
+  bookRow.appendChild(bookCard(newBook));
+  bookForm.reset();
+}
+
+function removeBookFromLibrary(event) {
+  event.target.parentElement.parentElement.remove();
+}
+
+function toggleRead(event) {
+  const targetID = event.target.getAttribute('data-target');
+  const toggleTarget = document.querySelector(`[data-id='${targetID}']`);
+  const readStatus = toggleTarget.querySelector('.readStatus');
+
+  event.target.checked ? readStatus.innerHTML = `Read: true` : readStatus.innerHTML = `Read: false`;
+}
 
 function bookCard(book) {
-  const domParser = new DOMParser()
-  const uuid = uuidv4()
+  const domParser = new DOMParser();
+  const uuid = uuidv4();
   const card = `
     <div class="col-6" data-id="${uuid}">
         <div class="card border-primary shadow h-100 position-relative overflow-hidden">
@@ -40,48 +75,12 @@ function bookCard(book) {
         </div>
       </div>
     </div>
-  `
-  return domParser.parseFromString(card, 'text/html').body.children[0]
+  `;
+  return domParser.parseFromString(card, 'text/html').body.children[0];
 }
 
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    let r = Math.random() * 16 | 0,
-      v = c == 'x' ? r : (r & 0x3 | 0x8)
-    return v.toString(16)
-  })
-}
+myLibrary.forEach((book) => {
+  bookRow.appendChild(bookCard(book));
+});
 
-function checker(book) {
-  return book.read ? 'checked' : ''
-}
-
-function toggleRead(event) {
-  const targetID = event.target.getAttribute('data-target')
-  const toggleTarget = document.querySelector(`[data-id='${targetID}']`)
-  const readStatus = toggleTarget.querySelector('.readStatus')
-
-  event.target.checked ? readStatus.innerHTML = `Read: true` : readStatus.innerHTML = `Read: false`
-}
-
-function addBookToLibrary(event) {
-  event.preventDefault()
-
-  const formData = new FormData(bookForm)
-  const newBook = new Book(
-    title = formData.get('title'),
-    author = formData.get('author'),
-    read = Boolean(formData.get('read')),
-    pages = formData.get('pages')
-  )
-
-  bookRow.appendChild(bookCard(newBook))
-  bookForm.reset()
-}
-
-function removeBookFromLibrary(event) {
-  event.target.parentElement.parentElement.remove()
-  console.log(event.target)
-}
-
-bookForm.addEventListener('submit', addBookToLibrary)
+bookForm.addEventListener('submit', addBookToLibrary);
