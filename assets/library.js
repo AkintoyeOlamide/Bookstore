0da3a65814e588acb1f277c12694c273e25bdc5a
CY Kalu <cy23@tutanota.com>
2021-02-23 16:23:52 +0100

    Fix linter errors

--- assets/library.js
+++ assets/library.js
@@ -12,38 +12,15 @@ function Book(title, author, read, pages) {
 myLibrary.push(
   new Book('The Politics', 'Sigmund F.', true, 442),
   new Book('The hobbit', 'JR.Jenkins', true, 786),
-  new Book('Hard kill', 'lotert runway', false, 235)
+  new Book('Hard kill', 'lotert runway', false, 235),
 );
 
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
-    const random = Math.random() * 16 | 0;
-    const v = char === 'x' ? random : (random & 0x3 | 0x8);
-    return v.toString(16);
-  });
+function randomID() {
+  return Math.floor((Math.random(425367) * 10000000000));
 }
 
 function checker(book) {
   return book.read ? 'checked' : '';
-}
-
-function addBookToLibrary(event) {
-  event.preventDefault();
-
-  const formData = new FormData(bookForm);
-  const newBook = new Book(
-    title = formData.get('title'),
-    author = formData.get('author'),
-    read = Boolean(formData.get('read')),
-    pages = formData.get('pages')
-  );
-
-  bookRow.appendChild(bookCard(newBook));
-  bookForm.reset();
-}
-
-function removeBookFromLibrary(event) {
-  event.target.parentElement.parentElement.remove();
 }
 
 function toggleRead(event) {
@@ -51,16 +28,25 @@ function toggleRead(event) {
   const toggleTarget = document.querySelector(`[data-id='${targetID}']`);
   const readStatus = toggleTarget.querySelector('.readStatus');
 
-  event.target.checked ? readStatus.innerHTML = `Read: true` : readStatus.innerHTML = `Read: false`;
+  readStatus.innerHTML = event.target.checked ? 'Read: true' : 'Read: false';
+}
+
+function removeBookFromLibrary(event) {
+  event.target.parentElement.parentElement.remove();
+}
+
+function addCardEventListeners(card) {
+  card.getElementsByClassName('book-delete')[0].addEventListener('click', removeBookFromLibrary);
+  card.getElementsByClassName('read-toggler')[0].addEventListener('change', toggleRead);
 }
 
 function bookCard(book) {
   const domParser = new DOMParser();
-  const uuid = uuidv4();
-  const card = `
+  const uuid = randomID();
+  const cardHTML = `
     <div class="col-6" data-id="${uuid}">
         <div class="card border-primary shadow h-100 position-relative overflow-hidden">
-        <button type="button" class="btn btn-danger rounded-0 py-0 position-absolute top-5 end-0" onclick="removeBookFromLibrary(event)">X</button>
+        <button type="button" class="book-delete btn btn-danger rounded-0 py-0 position-absolute top-5 end-0">X</button>
         <div class="card-body">
           <h6 class="card-title mb-0">${book.title}</h6>
           <p class="card-subtitle mb-3 text-muted">${book.author}</p>
@@ -68,15 +54,30 @@ function bookCard(book) {
           <div class="mb-0 d-flex">
             <span class="readStatus">Read: ${book.read}</span>
             <div class="form-check form-switch d-inline-flex ms-3">
-              <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" ${checker(book)} onchange="toggleRead(event)" data-target="${uuid}">
-              <label class="form-check-label" for="flexSwitchCheckChecked"></label>
+              <input class="form-check-input read-toggler" type="checkbox" id="readStatus" ${checker(book)} data-target="${uuid}">
+              <label class="form-check-label" for="readStatus"></label>
             </div>
           </div>
         </div>
       </div>
     </div>
   `;
-  return domParser.parseFromString(card, 'text/html').body.children[0];
+  const card = domParser.parseFromString(cardHTML, 'text/html').body.children[0];
+  addCardEventListeners(card);
+  return card;
+}
+
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const formData = new FormData(bookForm);
+  const title = formData.get('title');
+  const author = formData.get('author');
+  const read = Boolean(formData.get('read'));
+  const pages = formData.get('pages');
+  const newBook = new Book(title, author, read, pages);
+
+  bookRow.appendChild(bookCard(newBook));
+  bookForm.reset();
 }
 
 myLibrary.forEach((book) => {
